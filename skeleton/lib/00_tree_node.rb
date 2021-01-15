53b8943308c8323417e773defb13f264156d3848
Lane Harris <lrharris215@gmail.com>
2021-01-15 14:06:46 -0500

    Creates PolyTreeNode class and #initialize, #parent=, #add_child, #remove_child

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,41 @@
 class PolyTreeNode
 
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def value
+        @value
+    end
+
+    def parent
+        @parent
+    end
+
+    def children
+        @children
+    end
+
+    def parent=(new_parent)
+        old_parent = @parent
+        @parent = new_parent
+        unless @parent == nil
+            @parent.add_child(self) 
+        end
+        old_parent.remove_child(self) unless old_parent == nil
+    end
+
+    def add_child(new_child)
+       unless @children.include?(new_child)
+        @children << new_child 
+        new_child.parent = self
+       end
+    end
+
+    def remove_child(old_child)
+        children.delete(old_child)
+    end
+
 end
